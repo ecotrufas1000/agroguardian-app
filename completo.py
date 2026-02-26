48dd9b2e73b8a05d239cce89bb24ef4e921e6946
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-02-26 07:44:09 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -325,7 +325,7 @@ elif menu == "🌧️ Pluviómetro":
 elif menu == "💧 Balance Hídrico":
     import folium
     from streamlit_folium import folium_static
-    st.markdown("### 💧 ETo/Blanney-Criddle")
+    st.markdown("### 💧 Evapotranspiracion Blanney-Criddle")
     try:
         lat = LAT if LAT else -38.29
         lon = LON if LON else -57.55
