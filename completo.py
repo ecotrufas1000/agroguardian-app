0228d4868b76f4f9a4939898ea2f71477e2f14a3
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-01 18:05:44 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -844,7 +844,7 @@ elif menu == "🛰️ Índices Satelitales":
             token = get_sentinel_token()
             st.write(f"Token obtenido: {token is not None}")
         
-           if token:
+            if token:
                img_data = get_sentinel_image(token, evalscripts[indice], LAT, LON)
                st.write(f"Imagen obtenida: {img_data is not None}")
                st.write(f"LAT: {LAT}, LON: {LON}")
