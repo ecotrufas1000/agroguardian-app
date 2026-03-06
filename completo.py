3d6fc3cd8e28edd6d872b9b5419cc262f5eef70b
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-06 12:22:43 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -302,10 +302,9 @@ def obtener_clima_completo(lat, lon):
 #st.set_page_config(page_title="AgroGuardian Pro", layout="wide", page_icon="🛰️")
 st.set_page_config(
     page_title="AgroGuardian",
-    page_icon="https://raw.githubusercontent.com/ecotrufas1000/agroguardian-app/main/logo1.png",
+    page_icon="🌿",
     layout="wide"
 )
-
 
 st.markdown("""
     <style>
