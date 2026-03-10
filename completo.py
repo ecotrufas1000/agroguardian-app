f52e92fd4f3cc8a73c70596c86f1900dd11818b2
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-10 16:50:01 -0300

    foto

--- completo.py
+++ completo.py
@@ -978,8 +978,8 @@ elif menu == "🔍 Diagnóstico IA":
     # Galería abajo, fuera de tabs
     st.markdown("**📁 O subí una foto desde galería:**")
     img_upload = st.file_uploader("Seleccionar imagen", type=['jpg', 'jpeg', 'png'], label_visibility="collapsed")
-    if img_upload is not None:
-        st.session_state.foto_bytes = img_upload.read()
+    st.write("Estado:", img_upload)
+    st.write("foto_bytes en memoria:", st.session_state.foto_bytes is not None)
 
     # Mostrar imagen y botón analizar
     if st.session_state.foto_bytes:
