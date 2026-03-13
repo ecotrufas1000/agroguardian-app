06cc13bc02afd54fb22c175260c2e13f19b7e49b
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-13 09:23:29 -0300

    ll

--- completo.py
+++ completo.py
@@ -96,9 +96,9 @@ if st.session_state.usuario is None:
     <div style='text-align:center; padding:40px;'>
         <div style='display:flex; align-items:center; justify-content:center; gap:12px;'>
             <img src='https://raw.githubusercontent.com/ecotrufas1000/agroguardian-app/main/logo1.png' width='50px'>
-            <h1 style='color:#00ffc3; font-family:monospace; margin:0;font-size:22px;'>AgroGuardian</h1>
+            <h1 style='color:#00ffc3; font-family:monospace; margin:0;font-size:23px;'>AgroGuardian</h1>
         </div>
-        <p style='color:#888; font-family:monospace;font-size:19px;'>Precision Lab v2.6</p>
+        <p style='color:#888; font-family:monospace;font-size:15px;'>Precision Lab v2.6</p>
     </div>
     """, unsafe_allow_html=True)
 
