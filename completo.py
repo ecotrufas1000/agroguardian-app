cbeceb9cedc588a94969ba9a2d78b9c08e64a659
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-18 15:15:13 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -363,10 +363,10 @@ def cerrar_sesion():
 with st.sidebar:
     st.markdown(f"👤 **{st.session_state.get('usuario', '')}**")
     if st.button("🚪 Cerrar sesión"):
-    cerrar_sesion()
-    streamlit_js_eval(
-        js_expressions='localStorage.clear(); window.location.reload();',
-        key="ls_cerrar_y_recargar"
+        cerrar_sesion()
+        streamlit_js_eval(
+            js_expressions='localStorage.clear(); window.location.reload();',
+            key="ls_cerrar_y_recargar"
     )
 
 
