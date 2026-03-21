8c23d79d3657cbfcf8646581769111cd0dd7995a
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-21 18:36:10 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -563,7 +563,7 @@ def obtener_clima_completo(lat, lon):
             return {
                 "temp": t, "hum": h, "v_vel": round(r["wind"]["speed"] * 3.6, 1),
                 "v_dir": r["wind"].get("deg", 0), "rocio": round(rocio, 1),
-                "presion": r["main"]["pressure"], "localidad": r.get("name", "Zona Rural")
+                "presion": r["main"]["pressure"], "localidad": r.get("name", "Zona Rural"),
                 "nubes": r.get("clouds", {}).get("all", 0),
             }
     except:
