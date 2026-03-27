de93850dc5f76fd67cafdee2c1b9b4870506b981
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-03-27 11:38:15 -0300

    Update frutales.py

--- frutales.py
+++ frutales.py
@@ -2028,16 +2028,20 @@ if menu == "🛰️ Rend. Inteligente":
         # ================================
         m3 = folium.Map(location=[lat, lon], zoom_start=15)
 
+        # calcular rangos
+        min_val = min(rend_vals)
+        max_val = max(rend_vals)
+        
+        # evitar que sean iguales (muy importante)
+        if min_val == max_val:
+            max_val = min_val + 1
+        
+        # generar tile
         url_rend = rend_est.visualize(
-            min_val = min(rend_vals)
-            max_val = max(rend_vals)
-            
-            url_rend = rend_est.visualize(
-                min=min_val,
-                max=max_val,
-                palette=['blue', 'yellow', 'red']
-            ).getMapId()['tile_fetcher'].url_format
-
+            min=min_val,
+            max=max_val,
+            palette=['blue', 'yellow', 'red']
+        ).getMapId()['tile_fetcher'].url_format
         folium.TileLayer(
             tiles=url_rend,
             attr='GEE',
