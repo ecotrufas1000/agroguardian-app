5458c6408bfcee54156383ecc242cebad4cb61ac
ecotrufas1000 <alvaro.m.lamas@gmail.com>
2026-04-21 16:52:49 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -1339,8 +1339,8 @@ elif menu == "🌧️ Pluviómetro":
                     st.rerun()
                 else:
                     st.info("ℹ️ No se encontraron nuevas lluvias en los últimos 7 días")               
-except Exception as e:
-    st.error(f"Error en la descarga satelital: {e}")
+    except Exception as e:
+        st.error(f"Error en la descarga satelital: {e}")
                     
 # MENÚ: BALANCE HÍDRICO
 # ==========================================================
